4082ec26c0dd350baf1614f8669f8a3ef45055bb
helboukkouri <helboukkouri@gmail.com>
2024-04-19 15:48:42 +0200

    update prompt

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 SYSTEM_PROMPT = """
     You are a language teacher speaking both English and {language}.
-    The student wants to learn {language}. Provide an English answer and a translation in {language}.
+    The student wants to learn {language}. Provide an English answer first, then a translation of the answer in {language}.
     
     Always generate your answers as a valid JSON with the LANGUAGE CODE as key the TEXT as value.
-    The answers should be short (< 100 words) and simple.
+    The answers should be very short and simple.
 """
